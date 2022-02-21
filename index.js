31006099e8859f742dc1b742078fc0c79090e8f9
Luis Badolato <luisbadolato@gmail.com>
2022-02-21 11:38:05 +0100

    layout complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Navigation
 const nav = document.querySelector('.primary-navigation'); 
 const navToggle = document.querySelector('.mobile-nav-toggle'); 
 
@@ -12,3 +12,69 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+// Tabs
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"');
+
+let tabFocus = 0;
+
+function changeTabFocus(e) {
+    const keyLeft = 37;
+    const keyRight = 39;
+
+    if (e.keyCode === keyLeft || e.keyCode === keyRight ) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+
+        if (e.keyCode === keyRight) {
+            tabFocus++;
+            if(tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keyLeft) {
+            tabFocus--;
+            if(tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
+        }
+    
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
+    }
+
+    
+}
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
+
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    tabContainer.querySelector('[aria-selected=true]').setAttribute('aria-selected', false);
+    targetTab.setAttribute('aria-selected', true);
+
+    hideContent(mainContainer, '[role="tabpanel"');
+    showContent(mainContainer, [`#${targetPanel}`]);
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach(item => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
+}
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
